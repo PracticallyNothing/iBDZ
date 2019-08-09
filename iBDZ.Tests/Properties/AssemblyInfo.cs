7c99d53fee7c047a0005ed6a0a5b59a5d9d67d41
Practically Nothing <practicallynothingatall@gmail.com>
2019-08-09 22:13:53 +0300

    Added Unit testing project

---
+++ iBDZ.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("iBDZ.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("by adguard")]
+[assembly: AssemblyProduct("iBDZ.Tests")]
+[assembly: AssemblyCopyright("Copyright © by adguard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f64fbf2e-0873-4298-9162-ac05047cdfe6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
